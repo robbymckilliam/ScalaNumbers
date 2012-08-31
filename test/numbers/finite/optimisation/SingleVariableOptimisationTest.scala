3b46e25d792ca925cd673108f824de2075110179
harprobey <robby.mckilliam@gmail.com>
2012-08-31 15:55:06 +0930

    minor test edits.

--- test/numbers/finite/optimisation/SingleVariableOptimisationTest.scala
+++ test/numbers/finite/optimisation/SingleVariableOptimisationTest.scala
@@ -12,27 +12,44 @@ class SingleVariableOptimisationTest {
   def QuadraticTest() = {
     val f : Double => Double = x => x*x
     val (fx, x) = new Brent(f, -10.0, 8.0, 5.0, tol).min
-    println(fx, x)
+    //println(fx, x)
     assertEquals(0.0, fx, tol)
     assertEquals(0.0, x, tol)
     
     val (fxm, xm) = SingleVariableOptimisation.fmin(f, -11.0, 5.0, tol)
     assertEquals(0.0, fxm, tol)
     assertEquals(0.0, xm, tol)
-    
   }
   
   @Test
   def QuarticTest() = {
     val f : Double => Double = x => x*x*x*x
     val (fx, x) = new Brent(f, -2, 8.0, 10.0, tol).min
-    println(fx, x)
+    //println(fx, x)
     assertEquals(0.0, fx, tol)
     assertEquals(0.0, x, tol)
     
     val (fxm, xm) = SingleVariableOptimisation.fmin(f, -11.0, 5.0, tol)
     assertEquals(0.0, fxm, tol)
     assertEquals(0.0, xm, tol)
+    
+    val (fxmd, xmd) = SingleVariableOptimisation.fmin(f, -11.0, 5.0)
+    assertEquals(0.0, fxmd, 1e-6)
+    assertEquals(0.0, xmd, 1e-6)
+    
+  }
+  
+  @Test
+  def fmaxTest() = {
+    val f : Double => Double = x => -x*x
+    val (fxm, xm) = SingleVariableOptimisation.fmax(f, -11.0, 5.0, tol)
+    assertEquals(0.0, fxm, tol)
+    assertEquals(0.0, xm, tol)
+    
+    val (fxmd, xmd) = SingleVariableOptimisation.fmax(f, -11.0, 5.0)
+    assertEquals(0.0, fxmd, 1e-6)
+    assertEquals(0.0, xmd, 1e-6)
+    
   }
   
 }
