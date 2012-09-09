cb31caf5f02658c11f2ba0b50a91049ba6a2d477
harprobey <robby.mckilliam@gmail.com>
2012-09-09 15:15:17 +0930

    added bisection method for finding zeros

--- src/numbers/finite/optimisation/SingleVariableOptimisation.scala
+++ src/numbers/finite/optimisation/SingleVariableOptimisation.scala
@@ -19,7 +19,7 @@ object SingleVariableOptimisation {
    *@param f           The function to minimise 
    *@param  a         Left endpoint of initial interval
    *@param  c         Right endpoint of initial interval
-   *@param  b         bx must satisfy ax < bx < cx and f(ax) > f(bx) < f(cx)
+   *@param  b         b must satisfy a < b < c and f(a) > f(b) < f(c)
    *@param  tol         Desired length of the interval in which the minimum will be determined to lie (default 1e-6)
    *@params ITRMAX  maximum number of iterations before terminating (default 100)
    */
@@ -35,7 +35,7 @@ object SingleVariableOptimisation {
    *@param f           The function to maximise 
    *@param  a         Left endpoint of initial interval
    *@param  c         Right endpoint of initial interval
-   *@param  b         bx must satisfy ax < bx < cx and f(ax) > f(bx) < f(cx)
+   *@param  b         b must satisfy a < b < c and f(a) > f(b) < f(c)
    *@param  tol         Desired length of the interval in which the minimum will be determined to lie (default 1e-6)
    *@params ITRMAX  maximum number of iterations before terminating (default 100)
    */
@@ -44,32 +44,84 @@ object SingleVariableOptimisation {
     return (-minf, x)
   }
   
+  /** 
+   *Search for a zero of the function f(x) in the interval [a, b].
+   *Finds a solution such that |x0 - x| < tol where f(x0) = 0.
+   *Uses the bisection method, only guaranteed to converge if there is a unique zero 
+   *between a and b and f is continuous and sign(f(a)) = - sign(f(b))
+   *
+   *@param f           The function to zero 
+   *@param  a         Left endpoint of initial interval
+   *@param  c         Right endpoint of initial interval
+   *@param  b         bx must satisfy a < b < c and f(a) > f(b) < f(c)
+   *@param  tol         Desired length of the interval in which the minimum will be determined to lie (default 1e-6)
+   *@params ITRMAX  maximum number of iterations before terminating (default 100)
+   */
+  def fzero(f : Double => Double, a : Double, b: Double, tol : Double = 1e-6, ITRMAX : Int = 100) : Double = {
+    return new Bisection(f,a,b,tol,ITRMAX).zero
+  }
+  
+}
+
+/** 
+ *Search for a zero of the function f(x) in the interval [a, b].
+ *Finds a solution such that |x0 - x| < tol where f(x0) = 0.
+ *Uses the bisection method, only guaranteed to converge if there is a unique zero 
+ *between a and b and f is continuous and sign(f(a)) = - sign(f(b))
+ *
+ *@param f           The function to zero 
+ *@param  a         Left endpoint of initial interval
+ *@param  c         Right endpoint of initial interval
+ *@param  b         bx must satisfy a < b < c and f(a) > f(b) < f(c)
+ *@param  tol         Desired length of the interval in which the minimum will be determined to lie (default 1e-6)
+ *@params ITRMAX  maximum number of iterations before terminating (default 100)
+ */
+class Bisection(val f : Double => Double, val ax : Double, val bx : Double, val tol : Double, val ITRMAX : Int) {
+  
+  protected val xzero = run //run the optimiser
+  
+  /** Return (f(x), x) where f(x) = 0 */
+   def zero = xzero
+  
+   protected def run : Double = {
+      var a = ax; var b = bx
+      for( i <- 1 to ITRMAX ){
+        var c = (a + b)/2
+        var fc = f(c)
+        if( fc == 0 || (a-b).abs/2 < tol ) return c
+        if( sign(fc) == sign(f(a)) ) a = c else b = c
+      }
+      throw new RuntimeException("Bisection failed.  Maximum number " + ITRMAX + " of iterations exceeded")
+    }
+  
+   @inline protected final def sign(x : Double) : Double = scala.math.signum(x)
+
 }
 
 /**
- *Performs a 1-dimensional minimization.
- *It implements Brent's method which combines a golden-section
- *search and parabolic interpolation.  
- *
- * Ported from Numerical Recipes in C pages 404 - 405
- *
- *@param f           The function to minimise 
- *@param  ax         Left endpoint of initial interval
- *@param  cx         Right endpoint of initial interval
- *@param  bx         bx must satisfy ax < bx < cx and f(ax) > f(bx) < f(cx)
- *@param  tol         Desired length of the interval in which the minimum will be determined to lie
- *@params ITRMAX  maximum number of iterations before terminating
- */
-class Brent(f : Double => Double, ax : Double, bx : Double, cx: Double, tol : Double, ITRMAX : Int) {
+*Performs a 1-dimensional minimization.
+*It implements Brent's method which combines a golden-section
+*search and parabolic interpolation.  
+*
+* Ported from Numerical Recipes in C pages 404 - 405
+*
+*@param f           The function to minimise 
+*@param  ax         Left endpoint of initial interval
+*@param  cx         Right endpoint of initial interval
+*@param  bx         bx must satisfy ax < bx < cx and f(ax) > f(bx) < f(cx)
+*@param  tol         Desired length of the interval in which the minimum will be determined to lie
+*@params ITRMAX  maximum number of iterations before terminating
+*/
+class Brent(val f : Double => Double, val ax : Double, val bx : Double, val cx: Double, val tol : Double, val ITRMAX : Int) {
 
   protected val ZEPS = 1e-10 //close to machine precision
   protected val C = (3.0 - sqrt(5.0))/2.0;
-  
+
   protected val (fmin, xmin) = run //run the optimiser
-  
+
   /** Return (f(x), x) where f(x) is the minimum */
   def min = (fmin, xmin)
-  
+
   protected def run : (Double, Double) = {
     var e = 0.0
     var d = 0.0
@@ -129,7 +181,7 @@ class Brent(f : Double => Double, ax : Double, bx : Double, cx: Double, tol : Do
     println("Warning: Brent's method reached the maximum number " + ITRMAX + " iterations")
     return (fx, x)
   }
-  
-  protected final def sign(a : Double, b : Double) = abs(a)*signum(b)
-  
+
+  @inline protected final def sign(a : Double, b : Double) = abs(a)*signum(b)
+
 }
--- test/numbers/finite/optimisation/SingleVariableOptimisationTest.scala
+++ test/numbers/finite/optimisation/SingleVariableOptimisationTest.scala
@@ -2,6 +2,7 @@ package numbers.finite.optimisation
 
 import numbers.finite.optimisation.SingleVariableOptimisation.fmin
 import numbers.finite.optimisation.SingleVariableOptimisation.fmax
+import numbers.finite.optimisation.SingleVariableOptimisation.fzero
 
 import org.junit.Test;
 import org.junit.Assert._;
@@ -40,8 +41,30 @@ class SingleVariableOptimisationTest {
     
     val (fxmd, xmd) = fmax(f, -11.0,4.0,13.0)
     assertEquals(0.0, fxmd, 1e-6)
-    assertEquals(2.0, xmd, 1e-6)
+    assertEquals(2.0, xmd, 1e-6)    
+  }
+  
+  @Test
+  def fzeroLinearTest() = {
+    val f : Double => Double = x => x
+    val x = fzero(f, -11.0,8.0,tol)
+    assertEquals(0.0, f(x), tol)
+    assertEquals(0.0, x, tol)
+  }
+  
+  @Test
+  def fzeroCubicTest() = {
+    val f : Double => Double = x => x*x*(x-1)
+    val x1 = fzero(f, 0.5,1.7,tol)
+    println(x1,f(x1))
+    //assertEquals(0.0, f(x1), tol)
+    assertEquals(1.0, x1, tol)
     
+    //THIS CASE FAILS BECAUSE FZERO USES BISCECTION!
+    //val x2 = fzero(f, -0.5,0.7,tol)
+    //println(x2,f(x2))
+    //assertEquals(0.0, f(x2), tol)
+    //assertEquals(0.0, x2, tol)
   }
   
 }
