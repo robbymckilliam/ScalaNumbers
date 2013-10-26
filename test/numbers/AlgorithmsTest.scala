857373af64d570999aea8cc7527a1904b5b9dcc6
robbymckilliam <robbymckilliam@gmail.com>
2013-10-26 14:42:57 +1030

    added test for Algorithms

---
+++ test/numbers/AlgorithmsTest.scala
@@ -0,0 +1,22 @@
+/*
+ * @author Robby McKilliam
+ */
+
+package numbers
+
+import org.junit.Test
+import org.junit.Assert._
+
+class AlgorithmsTest {
+  
+   @Test def catchMaxIterationsException = {
+    val convitr =  new ConvergentIteration[Double](1.0, x=>x+1.0, (x,y) => false, 20) 
+    try{
+      val c = convitr.limit
+     }catch{
+       case e : convitr.MaximumIterationsReachedException => println(e.getMessage + " Limit was " + e.limit)
+       case _ : Throwable => fail("exception not correctly caught")
+     }
+  }
+
+}
